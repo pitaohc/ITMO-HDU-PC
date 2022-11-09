15c3e10e89648e2fa0d5c33576d16da9dc7574da
pc <pitaohc@qq.com>
2022-11-08 18:53:14 -0800

    temp

---
+++ lab1/lab1.c
@@ -0,0 +1,133 @@
+//
+// Created by pc on 11/1/22.
+//
+
+#include <stdlib.h>
+#include <sys/time.h>
+#include <stdio.h>
+#include <math.h>
+
+#define A 24
+#define SEED 34
+#define DEFAULT_N 10
+
+double* fill_array(double *arr, size_t size, unsigned int min, unsigned int max);
+void map1(double *arr, size_t size);
+void map2(double *arr, size_t size);
+double reduce(double *arr, size_t size);
+void merge(double *arr1, double *arr2, size_t size2);
+void stupid_sort(double *arr, size_t size);
+void print_array(double *arr, size_t size,char* message);
+int main(int argc, char* argv[]) {
+    struct timeval T1, T2;
+    long time_ms, minimal_time_ms = -1;
+    size_t N;
+    if(argc > 1)
+        N = (size_t) atoi(argv[1]);  /* инициализировать число N первым параметром командной строки */
+    else
+        N = DEFAULT_N;
+    double x, *m1 = (double*)malloc(sizeof(double) * N), *m2 = (double*)malloc(sizeof(double) * (N / 2));
+
+    for (size_t i = 0; i < 1; ++i) {
+        gettimeofday(&T1, NULL); /* запомнить текущее время T1 */
+        fill_array(m1, N, 0, A);
+        fill_array(m2, N/2, A, 10*A);
+        print_array(m1,N,"m1");
+        print_array(m2,N,"m2");
+
+        map1(m1, N);
+        map2( m2, N/2);
+        merge(m1, m2, N/2);
+        stupid_sort(m2, N/2);
+        print_array(m1,N,"m1");
+        print_array(m2,N,"m2");
+        x = reduce(m2, N/2);
+        gettimeofday(&T2, NULL); /* запомнить текущее время T2 */
+        time_ms = 1000 * (T2.tv_sec - T1.tv_sec) + (T2.tv_usec - T1.tv_usec) / 1000;
+        if ((minimal_time_ms == -1) || (time_ms < minimal_time_ms))
+            minimal_time_ms = time_ms;
+    }
+
+    free(m1);
+    free(m2);
+
+    printf("Best time: %ld ms; N = %zu; X = %f\n", minimal_time_ms, N, x); /* затраченное время */
+    return 0;
+}
+
+double* fill_array(double *arr, size_t size, unsigned int min, unsigned int max)
+{
+    unsigned int seed = SEED;
+    for (size_t i = 0; i < size; i++)
+        arr[i] = ((double) (rand_r(&seed)%(100*(max-min)))/100) + min;
+    return arr;
+}
+
+
+void map1(double *arr, size_t size) {
+    for (size_t i = 0; i < size; i++) {
+        arr[i]= sqrt(1.0/ tanh(arr[i]));
+    }
+}
+
+void map2(double *arr,size_t size)
+{
+    double prev = 0.0f;
+    for (size_t i = 0; i < size; i++) {
+        prev = arr[i]= fabs(sin(arr[i]+prev));
+    }
+}
+
+void merge(double *arr1, double *arr2, size_t size2) {
+    for (size_t i = 0; i < size2; i++) {
+        arr2[i] = pow(arr1[i], arr2[i]);
+    }
+}
+
+
+void stupid_sort(double *arr, size_t size) {
+    size_t i = 0;
+    double tmp;
+    while (i < size - 1) {
+        if (arr[i + 1] < arr[i]) {
+            tmp = arr[i];
+            arr[i] = arr[i + 1];
+            arr[i + 1] = tmp;
+            i = 0;
+        } else
+            i++;
+    }
+//    return 0;
+}
+
+
+double reduce(double *arr, size_t size) {
+    double res = 0, min;
+    size_t i = 0;
+
+    if (size > 0)
+        min = arr[0];
+    else
+        min = 0;
+    for (i = 0; i < size; i++) {
+        if (arr[i] < min && arr[i] != 0) {
+            min = arr[i];
+        }
+    }
+
+    for (i = 0; i < size; i++) {
+        if ((int)(arr[i] / min) % 2 == 0) {
+            res += sin(arr[i]);
+        }
+    }
+
+    return res;
+}
+
+void print_array(double *arr, size_t size,char* message) {
+    printf("%s\n",message);
+    for (size_t i = 0; i < size; ++i) {
+        printf("%f ",arr[i]);
+    }
+    printf("\n");
+}
