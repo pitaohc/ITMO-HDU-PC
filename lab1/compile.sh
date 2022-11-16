a28949683c478e4f8b648c51aefcbdb71b1e5427
pitao <pitaohc@qq.com>
2022-11-16 20:09:37 +0800

    添加注释 add annotates

--- lab1/compile.sh
+++ lab1/compile.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
+# author: pitaohc@qq.com
+threads=(1 4 8 16)	# 多线程优化的线程数
+N1=235		# 10ms N1
+N2=1575		# 2000ms N2
+Compiler=gcc	# 编译器
 
-threads=(1 4 8 16)
-N1=235
-N2=1575
-Compiler=gcc
-
+# 创建构建目录
 if [ ! -d "./build_$Compiler" ]; then #if build doesn't exist, then create it
 	mkdir build_$Compiler
 fi
 cd build_$Compiler
 
-# compile all targets
-$Compiler -O3 -Wall -Werror -o lab1-seq ../lab1.c -lm
+# 编译目标 compile all targets
+$Compiler -O3 -Wall -Werror -o lab1-seq ../lab1.c -lm # 编译无并行目标
 if [ $? -eq 0 ];then
     echo 'Compile seq success!!!'
-    programs=("${programs[@]}" lab1-seq)
+    programs=("${programs[@]}" lab1-seq) # 添加程序文件名称到程序数组
 fi
 
+# 编译并行目标 compile parallel target
 for loop in ${threads[@]}
 do
-    $Compiler -O3 -Wall -Werror -floop-parallelize-all -ftree-parallelize-loops=$loop ../lab1.c -o lab1-par-$loop -lm
+    $Compiler -O3 -Wall -Werror -floop-parallelize-all -ftree-parallelize-loops=$loop ../lab1.c -o lab1-par-$loop -lm 
     if [ $? -eq 0 ];then
         echo "Compile lab1-par-${loop} success!!!"
         programs=("${programs[@]}" lab1-par-${loop})
@@ -28,28 +30,27 @@ done
 
 echo "programs: ${programs[@]}"
 
-# run test
-step=$((($N2-$N1)/10))
+# 运行测试 run test
+step=$((($N2-$N1)/10)) # 步长
 echo "N1=$N1, N2=$N2, step=$step"
 
+# 创建日志文目录 create log directory
 if [ -d "./log" ]; then #if log exist, then remove it
     rm -rf log
 fi
 mkdir log
 
-
+# 运行程序 execute programs
 for program in ${programs[@]}
 do
     echo "$program:"
-    echo "$program:" >> log/total.log
-    for i in $(seq 11)
+    echo "$program:" >> log/total.log # 输出程序名称到总日志
+    for i in $(seq 11) # 每个程序测试11次 run program by different N
     do
         N=$(($N1+$step*($i-1)))
         echo "test $i: N=$N"
-	echo -e "\ntest $i: N=$N" >> log/$program.log
-	./$program $N >> log/$program.log
+        echo -e "\ntest $i: N=$N" >> log/$program.log
+        ./$program $N >> log/$program.log # input N to program as argument 将N作为参数输入到程序中
     done
-    cat log/$program.log | grep "Milliseconds passed" >> log/total.log
+    cat log/$program.log | grep "Milliseconds passed" >> log/total.log # 截取程序输出中的时间关键字Milliseconds passed并记录到总日志中
 done
-
-
--- lab1/lab1.c
+++ lab1/lab1.c
@@ -1,5 +1,4 @@
-//
-// Created by pc on 11/1/22.
+// Created by pitaohc on 11/1/22.
 //
 
 #include <stdlib.h>
@@ -18,39 +17,44 @@ double reduce(double *arr, size_t size);
 void merge(double *arr1, double *arr2, size_t size2);
 void stupid_sort(double *arr, size_t size);
 void print_array(double *arr, size_t size,char* message);
+
 int main(int argc, char* argv[]) {
-    struct timeval T1, T2;
-    long delta_ms=0;
-    int N;
+    struct timeval T1, T2; //record time
+    long delta_ms=0; // 时间差值 T2-T1
+    int N; // size of array
     if(argc > 1)
     {
-        N = (size_t) atoi(argv[1]);  //读取参数
+        N = (size_t) atoi(argv[1]);  //get N from argument 读取参数
     }
     else
     {
-        N = DEFAULT_N;
+        N = DEFAULT_N; //if not argument , set N by default
     }
     double x, *m1 = (double*)malloc(sizeof(double) * N), *m2 = (double*)malloc(sizeof(double) * (N / 2));
     printf("N=%d\n",N);
     gettimeofday(&T1, NULL);
     for (int i = 0; i < 50; ++i) {
+        // Generate Stage.
         fill_array(m1, N, 0, A);
         fill_array(m2, N/2, A, 10*A);
-
+        // Map Stage.
         map1(m1, N);
         map2( m2, N/2);
-
+        // Merge Stage.
         merge(m1, m2, N/2);
+        // Sort Stage.
         stupid_sort(m2, N/2);
+        // Reduce Stage.
         x = reduce(m2, N/2);
-
+        // Output X
         fprintf(stdout,"%d => X:%f\n",i+1,x);
     }
 
-    gettimeofday(&T2, NULL); /* запомнить текущее время T2 */
-    delta_ms = 1000 * (T2.tv_sec - T1.tv_sec) + (T2.tv_usec - T1.tv_usec) / 1000;
-
+    gettimeofday(&T2, NULL);
+    // Output delta time(ms)
+    delta_ms = 1000 * (T2.tv_sec - T1.tv_sec) + (T2.tv_usec - T1.tv_usec) / 1000; 
     printf("N=%d. Milliseconds passed: %ld\n", N, delta_ms); /* T2 -T1 */
+    // Release memory distributed by malloc.
     free(m1);
     free(m2);
     return 0;
@@ -58,13 +62,18 @@ int main(int argc, char* argv[]) {
 
 /*
 * 填充数组
+* fill an array by random number in [min,max]
+* @param arr 
+* @param size: length of array
+* @param min: mininum of random range
+* @param max: maxinum of random range
 */
 double* fill_array(double *arr, size_t size, unsigned int min, unsigned int max)
 {
 //    unsigned int seed = SEED;
     struct  timeval time;
     gettimeofday(&time,NULL);
-    unsigned int seed = time.tv_usec+time.tv_sec;
+    unsigned int seed = time.tv_usec+time.tv_sec; // use current time as random seed.
     for (int i = 0; i < size; i++)
         arr[i] = ((double) (rand_r(&seed)%(100*(max-min)))/100) + min;
     return arr;
@@ -72,16 +81,21 @@ double* fill_array(double *arr, size_t size, unsigned int min, unsigned int max)
 
 /*
 * 使用方法1映射数组
+* map elements in array by hyperbolic cotangent of number’s root
+* @param arr
+* @param size: length of array
 */
 void map1(double *arr, size_t size) {
     for (int i = 0; i < size; i++) {
-        arr[i]= sqrt(1.0/ tanh(arr[i]));
-//        arr[i]= sqrt(1.0/ tanh(arr[i]));
+        arr[i] = 1.0 / tanh(sqrt(arr[i]));
     }
 }
 
 /*
 * 使用方法2映射数组
+* map elements in array by sine modulus
+* @param arr
+* @param size: length of array
 */
 void map2(double *arr,size_t size)
 {
@@ -93,6 +107,10 @@ void map2(double *arr,size_t size)
 
 /*
 * 合并阶段
+* merge stage: Raising to a power
+* @param arr1: array 1
+* @param arr2: array 2
+* @param size2: length of array 2
 */
 void merge(double *arr1, double *arr2, size_t size2) {
     for (int i = 0; i < size2; i++) {
@@ -102,6 +120,9 @@ void merge(double *arr1, double *arr2, size_t size2) {
 
 /*
 * 排序阶段
+* sort stage: stupid sort
+* @param arr
+* @param size: length of array
 */
 void stupid_sort(double *arr, size_t size) {
     int i = 0;
@@ -115,23 +136,26 @@ void stupid_sort(double *arr, size_t size) {
         } else
             i++;
     }
-//    return 0;
 }
 
 /*
 * 累加阶段
+* reduce stage: get the mininum number of array. if floor(arr[i]/min) is even, then the sum adds it's sine.
+* @param arr
+* @param size: length of array
 */
 double reduce(double *arr, size_t size) {
     double res = 0, min;
     int i = 0;
     min = arr[0];
-
+    // find the mininum number which not equal 0.
     for (i = 0; i < size; i++) {
         if (arr[i] < min && (int)arr[i] != 0) {
             min = arr[i];
         }
     }
 
+    // reduce
     for (i = 0; i < size; i++) {
         if ((int)(arr[i] / min) % 2 == 0) {
             res += sin(arr[i]);
@@ -142,6 +166,10 @@ double reduce(double *arr, size_t size) {
 
 /*
 * 打印数组
+* debug function: print the array
+* @param arr
+* @param size: length of array
+* @param message: extra message
 */
 void print_array(double *arr, size_t size,char* message) {
     printf("%s\n",message);
